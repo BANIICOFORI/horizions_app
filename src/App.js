a3692effa3cc338843cec070362cee9be6a347d4
BANIICOFORI <baniicofori@gmail.com>
2022-07-16 01:46:22 +0000

    Horizens app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Plans from './components/Plans';
+import Slider from './components/Slider';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+    <Navbar />
+     <Slider />
+     <Plans />
+     <Footer />
+    </div>
+  );
+}
+export default App;
